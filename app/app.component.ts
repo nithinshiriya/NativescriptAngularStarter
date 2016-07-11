7caa0d0eef723f261d19d42e3be447b67ff4b4f1
Nitheen Rao <nithinshiriys@gmail.com>
2016-07-11 13:53:27 -0300

    Http Post test.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { RouterConfig, ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
 import { NS_ROUTER_DIRECTIVES, nsProvideRouter} from "nativescript-angular/router"
+import {Http, Headers, Response, } from "@angular/http";
+import {Observable} from "rxjs";
 
 @Component({
     selector: "first",    
@@ -47,6 +49,7 @@ class SecondComponent implements OnInit, OnDestroy {
     template: `
         <StackLayout>
             <StackLayout class="nav">
+                <Button text="HTTP POST" (tap)="vote()"></Button>
                 <Button text="First" nsRouterLink="/"></Button>
                 <Button text="Second(1)" nsRouterLink="/second/1"></Button>
                 <Button text="Second(2)" [nsRouterLink]="['/second', '2' ]"></Button>
@@ -56,6 +59,32 @@ class SecondComponent implements OnInit, OnDestroy {
     `
 })
 export class RouterOutletAppComponent {
+
+    constructor(private _http: Http){}
+
+    vote(){
+       var headers: Headers;
+       headers = new Headers();
+       headers.append('Content-Type', 'text/plain');
+       var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoidXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjEwNDQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9hdXRoZW50aWNhdGlvbiI6ImV2ZXJ2YXN0X2Z1bGwiLCJpc3MiOiJFdmVydmFzdC4xMjNNZUFwcCIsImF1ZCI6Imh0dHA6Ly93d3cuMTIzbWVhcHAuY29tIiwiZXhwIjoxNjI1NjY4ODUxLCJuYmYiOjE0Njc5MDI0NTF9.IWnvqyk-nYtCehsJI9IeY82PofFsHNWG3FtMVhjSaXI";      
+       headers.append('Tocken', token);           
+
+        var url =  "http://service.picpollapp.com/PollService.svc/rest/AddPollAnswer?PollID=1008&OptionID=3469"              
+
+        this._http.post(url, null, {headers:  headers})        
+        .map((res) => res.json())
+        .catch(this.handleError)
+        .subscribe(
+            (data) => console.log("Sucess: " + data),
+            (Error) => console.log("Error: " + Error),
+            () => console.log("done")                        
+        )  
+    }
+
+    handleError(error: Response) {
+        return Observable.throw(error.json() || 'Server error');
+    }
+
 }
 
 
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {RouterOutletAppComponent, RouterOutletRouterProviders} from "./app.component";
+import {HTTP_PROVIDERS} from "@angular/http";
 
-nativeScriptBootstrap(RouterOutletAppComponent, [RouterOutletRouterProviders]);
+nativeScriptBootstrap(RouterOutletAppComponent, [HTTP_PROVIDERS, RouterOutletRouterProviders]);
